63434e0310906835b6ae40aff974847e3ed18a8a
Saray Cotes <saraycotes@gmail.com>
2023-05-25 11:33:24 -0500

    cambiar colores

--- ui/theme/Color.kt
+++ ui/theme/Color.kt
@@ -2,13 +2,13 @@ package com.cristhianbonilla.oraculo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFF8763C4)
-val PurpleGrey80 = Color(0xFFA5A0B5)
-val Pink80 = Color(0xFFC86F8B)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF4B2A71)
-val PurpleGrey40 = Color(0xFF7B7487)
-val Pink40 = Color(0xFF9D576C)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
 val Black = Color(0xFF000000)
 val SemiTransparentBlack = Color(0x80000000)
 
